5e6520e38f97aaebcfc6156e47ba42a6bff69ee7
Alankriti Srivastava <131175588+alankriti2@users.noreply.github.com>
2024-03-12 22:51:35 +0530

    Create Power of 2.java
    Given a non-negative integer N. The task is to check if N is a power of 2. More formally, check if N can be expressed as 2x for some integer x. Return true if N is power of 2 else return false.
    
    Example 1:
    
    Input: 
    N = 8
    Output: 
    YES
    Explanation:
    8 is equal to 2 raised to 3 (23 = 8).
    Example 2:
    
    Input: 
    N = 98
    Output: 
    NO
    Explanation: 
    98 cannot be obtained by any power of 2.
    Your Task:Your task is to complete the function isPowerofTwo() which takes n as a parameter and returns true or false by checking if the given number can be represented as a power of two or not.
    
    Expected Time Complexity:O(log N).
    Expected Auxiliary Space:O(1).

---
+++ Power of 2.java
@@ -0,0 +1,17 @@
+class Solution{
+    
+    // Function to check if given number n is a power of two.
+    public static boolean isPowerofTwo(long n){
+        
+        // Your code here
+        if(n==0)
+        {
+            return false;
+        }
+        else
+        {
+            return (n&(n-1))==0;
+        } 
+    }
+    
+}
